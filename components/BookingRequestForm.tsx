16f64a7b12342d28d0b7840707f9d292bdf08f8c
Jeff Shockey <jeffshockey90@gmail.com>
2026-06-22 18:20:37 -0400

    Expand booking request time selection to 8am-5pm

--- components/BookingRequestForm.tsx
+++ components/BookingRequestForm.tsx
@@ -14,9 +14,26 @@ const SERVICE_OPTIONS = [
 ];
 
 const TIME_OPTIONS = [
+  { label: "Select a time", value: "" },
+  { label: "8:00 AM", value: "08:00" },
+  { label: "8:30 AM", value: "08:30" },
+  { label: "9:00 AM", value: "09:00" },
+  { label: "9:30 AM", value: "09:30" },
   { label: "10:00 AM", value: "10:00" },
+  { label: "10:30 AM", value: "10:30" },
+  { label: "11:00 AM", value: "11:00" },
+  { label: "11:30 AM", value: "11:30" },
+  { label: "12:00 PM", value: "12:00" },
+  { label: "12:30 PM", value: "12:30" },
+  { label: "1:00 PM", value: "13:00" },
+  { label: "1:30 PM", value: "13:30" },
   { label: "2:00 PM", value: "14:00" },
+  { label: "2:30 PM", value: "14:30" },
+  { label: "3:00 PM", value: "15:00" },
+  { label: "3:30 PM", value: "15:30" },
+  { label: "4:00 PM", value: "16:00" },
   { label: "4:30 PM", value: "16:30" },
+  { label: "5:00 PM", value: "17:00" },
   { label: "Flexible / Call Me", value: "Flexible" },
 ];
 
@@ -122,9 +139,9 @@ export default function BookingRequestForm() {
     zip: "",
     square_feet: "",
     preferred_date: "",
-    preferred_time: "10:00",
+    preferred_time: "",
     alternate_date: "",
-    alternate_time: "14:00",
+    alternate_time: "",
     notes: "",
   });
 
